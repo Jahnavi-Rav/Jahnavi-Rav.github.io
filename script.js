c61c8a8b231088f881dacf305ee4ba8f6a49a0b4
Jahnavi-Rav <65764268+Jahnavi-Rav@users.noreply.github.com>
2025-01-14 19:31:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+<script>
 let currentPage = 1;
 
-document.getElementById('next').addEventListener('click', () => {
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+const page1 = document.getElementById('page1');
+const page2 = document.getElementById('page2');
+
+nextButton.addEventListener('click', () => {
     if (currentPage === 1) {
-        document.getElementById('page1').style.transform = 'rotateY(-180deg)';
+        page1.style.transform = 'rotateY(-180deg)';
+        page2.style.transform = 'rotateY(0deg)';
         currentPage = 2;
     }
 });
 
-document.getElementById('prev').addEventListener('click', () => {
+prevButton.addEventListener('click', () => {
     if (currentPage === 2) {
-        document.getElementById('page1').style.transform = 'rotateY(0deg)';
+        page1.style.transform = 'rotateY(0deg)';
+        page2.style.transform = 'rotateY(-180deg)';
         currentPage = 1;
     }
 });
+</script>
